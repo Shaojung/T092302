3a7c3d4f41e7c190b0485bb578f0b2f5064d9531
Shaojung <abc@abc.com>
2016-09-23 20:24:03 +0800

    Add ViewHolder

--- app/src/main/java/tw/com/pcschool/t092302/MyAdapter.java
+++ app/src/main/java/tw/com/pcschool/t092302/MyAdapter.java
@@ -2,6 +2,7 @@ package tw.com.pcschool.t092302;
 
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,13 +20,14 @@ public class MyAdapter extends BaseAdapter {
     String[] prices;
     int[] imgs;
     Context context;
+    LayoutInflater inflater;
     public MyAdapter(Context c, String[] fruits, String[] p, int[] i)
     {
         this.fruits = fruits;
         this.prices = p;
         this.imgs = i;
         context = c;
-
+        inflater = ((AppCompatActivity) context).getLayoutInflater();
     }
     @Override
     public int getCount() {
@@ -44,15 +46,33 @@ public class MyAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        LayoutInflater inflater = ((AppCompatActivity) context).getLayoutInflater();
-        View v = inflater.inflate(R.layout.myitem, null);
-        TextView tv = (TextView) v.findViewById(R.id.textView);
-        tv.setText(fruits[position]);
-        TextView tv2 = (TextView) v.findViewById(R.id.textView2);
-        tv.setText(prices[position]);
-        ImageView img = (ImageView) v.findViewById(R.id.imageView);
-        img.setImageResource(imgs[position]);
+        ViewHolder holder;
+        if (convertView == null)
+        {
+            convertView = inflater.inflate(R.layout.myitem, null);
+            holder = new ViewHolder();
+            holder.tv = (TextView) convertView.findViewById(R.id.textView);
+            holder.tv2 = (TextView) convertView.findViewById(R.id.textView2);
+            holder.img = (ImageView) convertView.findViewById(R.id.imageView);
+            convertView.setTag(holder);
+        }
+        else
+        {
+            holder = (ViewHolder) convertView.getTag();
+        }
 
-        return v;
+
+        holder.tv.setText(fruits[position]);
+        holder.tv2.setText(prices[position]);
+        holder.img.setImageResource(imgs[position]);
+
+
+        return convertView;
+    }
+    static class ViewHolder
+    {
+        TextView tv;
+        TextView tv2;
+        ImageView img;
     }
 }
